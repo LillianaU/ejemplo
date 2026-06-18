909cbeef9ce35d447dff3b494e335d174e69e0e1
Emmanuel <zapataarenasemanuel7@gmail.com>
2026-06-18 13:45:08 -0500

    movi el script js de html a script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const appear = document.querySelectorAll(".appear");
+
+window.addEventListener("scroll", () => {
+    appear.forEach((element) => {
+        const top = element.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+});
